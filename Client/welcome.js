90817aa963bc2ba5418a6e135194640671e81e72
tdelpidio <tdelpidio@gmail.com>
2022-07-12 16:16:16 -0400

    debugging

--- Client/welcome.js
+++ Client/welcome.js
@@ -14,41 +14,50 @@ const emailInput = document.getElementById('email');
 const nameInput = document.getElementById('name')
 
 
-const userEnter = (body) => {
-    axios.post('https://td-capstone.herokuapp.com/enter', body)
-    .then(res => {
-        if (res.data.success) {
-        } else {
-            console.log('Error with new user')
-        }
-    })
-}
-
-function submitHandler(event) {
-    event.preventDefault()
-
-    let body = {
-        name: nameInput.value,
-        email: emailInput.value
-    }
-
-    nameInput.value = ""
-    emailInput.value = ""
-
-    userEnter(body)
-}
-
-enterBtn.addEventListener('submit', submitHandler)
-
-// enterBtn.addEventListener('submit', (e) => {
-//     e.preventDefault()
-
-//     let name = nameInput.value
-//     let email = emailInput.value
-
-//     axios.post('https://td-capstone.herokuapp.com/enter', {name: name, email: email})
-//     .then(() => {
-//         console.log('working')
+// const userEnter = (body) => {
+//     axios.post('https://td-capstone.herokuapp.com/enter', body)
+//     .then(res => {
+//         if (res.data.success) {
+//         } else {
+//             console.log('Error with new user')
+//         }
 //     })
+// }
 
-// })
+// function submitHandler(event) {
+//     event.preventDefault()
+
+//     let body = {
+//         name: nameInput.value,
+//         email: emailInput.value
+//     }
+
+//     nameInput.value = ""
+//     emailInput.value = ""
+
+//     userEnter(body)
+// }
+
+// enterBtn.addEventListener('submit', submitHandler)
+
+
+let body = {
+    name: nameInput.value,
+    email: emailInput.value
+}
+
+nameInput.value = ""
+emailInput.value = ""
+
+enterBtn.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let name = nameInput.value
+    let email = emailInput.value
+
+    axios.post('https://td-capstone.herokuapp.com/enter', body)
+    .then(() => {
+        console.log('working')
+    })
+
+})
