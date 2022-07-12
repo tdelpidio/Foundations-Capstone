584af7f8dd0245c22f75edca0e632b7a81558833
tdelpidio <tdelpidio@gmail.com>
2022-07-11 22:28:53 -0400

    animation removed

--- Server/index.js
+++ Server/index.js
@@ -29,4 +29,6 @@ app.post('/enter', enter)
 
 const port = process.env.PORT || 4000
 
-app.listen(process.env.PORT || 4000);
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
