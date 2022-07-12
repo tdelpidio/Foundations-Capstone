8f6e9086665ee5b4c6cc53521a07848046161341
tdelpidio <tdelpidio@gmail.com>
2022-07-11 23:24:58 -0400

    corrected axios in main js

--- Client/main.js
+++ Client/main.js
@@ -12,7 +12,7 @@ const enterURL = `http://localhost:4000`
 
 
 fortuneBtn.addEventListener("click", () => {
-    axios.get('http://localhost:4000/getFortune').then(res => {
+    axios.get('/getFortune').then(res => {
         const messageElement = document.createElement('div')
         messageElement.innerHTML = `<p class="fortune">${res.data}</p>`
         messageDiv.appendChild(messageElement)
@@ -33,7 +33,7 @@ askBtn.addEventListener('click', () => {
     submitDiv.appendChild(submitBtn)
     
     submitBtn.addEventListener('click', () => {
-        axios.get('http://localhost:4000/getAnswer').then(res => {
+        axios.get('/getAnswer').then(res => {
             const messageElement = document.createElement('div')
             messageElement.innerHTML = `<p class="fortune">${res.data}</p>`
             messageDiv.appendChild(messageElement)
